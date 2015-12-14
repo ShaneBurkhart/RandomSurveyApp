90bf70966fceb2d89221b180283679974ce18a77
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-14 14:44:46 -0600

    Created a simple hello world server.

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+http.createServer(app).listen(3000);
