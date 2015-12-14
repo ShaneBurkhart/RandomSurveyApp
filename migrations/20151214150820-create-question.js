9c4d280e2b45bb7d81b6111814cadbd24668ec72
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-14 17:05:36 -0600

    Created migration for questions.

---
+++ migrations/20151214150820-create-question.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      question: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Questions');
+  }
+};
