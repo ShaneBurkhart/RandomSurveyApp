00c62289c367eacc7938bf4e223917c270e92cf1
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-15 17:40:03 -0600

    Cleaned up test with helpers.

--- spec/models/question_spec.js
+++ spec/models/question_spec.js
@@ -1,50 +1,39 @@
 'use strict'
 
 var db = require('../../models/db.js');
-var Question = db.question;
 
+var Answer = db.answer;
 describe('Question', function() {
   it('saves a record correctly', function(done) {
-    var question = Question.build({
-      question: 'What is your name?'
-    });
-
-    question.save().then(function(q) {
+    this.createDefaultQuestion('My name is Ron Burgundy?').then(function(q) {
       expect(q.isNewRecord).toBe(false);
       done();
-    }).catch(function(err) {
-      expect(err).toBeUndefined();
+    }).catch(this.createCheckHasNoErrorsCallback(done));
+  });
+
+  it('has many answers', function(done) {
+    var question;
+    var self = this;
+    this.createDefaultQuestion().then(function(q) {
+      // There is probably a better way to save the question for later promises,
+      // but this will do for now.
+      question = q;
+      return self.createAnswer('Some answer', question.id);
+    }).then(function(a1) {
+      return self.createAnswer('Some other answer', question.id);
+    }).then(function(a2) {
+      return question.getAnswers();
+    }).then(function(answers) {
+      expect(answers.length).toBe(2);
       done();
-    });
-  })
+    }).catch(this.createCheckHasNoErrorsCallback(done));
+  });
 
   it('doesn\'t allow null questions', function(done) {
-    var question = Question.build({
-      question: null
-    });
-
-    // We have to actually save the record to check the allowNull validation.  It isn't
-    // an instance validator and instead a Schema validator, so it doesn't fail until we
-    // try to save to the db.
-    question.save().then(function(q) {
-      // This is just to get a more clear error message. If we get here, the
-      // test has failed.
-      expect(q).toBeUndefined();
-      done();
-    }).catch(function(err) {
-      expect(err).not.toBeUndefined();
-      done();
-    });
-  })
+    this.checkQuestionIsNotValid(null, done);
+  });
 
   it('doesn\'t allow empty questions', function(done) {
-    var question = Question.build({
-      question: ''
-    });
-
-    question.validate().then(function(errors) {
-      expect(errors).not.toBeUndefined();
-      done();
-    });
-  })
+    this.checkQuestionIsNotValid('', done);
+  });
 });
