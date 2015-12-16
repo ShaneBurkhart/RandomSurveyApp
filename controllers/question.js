56690af3c247a5719a2cf5786a10528ed8f7066e
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-16 15:34:29 -0600

    Finished question#show path and test.

--- controllers/question.js
+++ controllers/question.js
@@ -1,12 +1,44 @@
 'use strict'
 
-var db = require('../../models/db.js');
+var db = require('../models/db.js');
 var Question = db.question;
 
 var QuestionController = {
   // Shows a random survey question that the user has not seen yet.
   show: function(req, res) {
+    var alreadySeen = req.cookies.alreadySeen || [];
+    findRandomQuestion(alreadySeen).then(function(q) {
+      res.render('question/show', {
+        question: q
+      });
+    });
   }
 }
+
+var findRandomQuestion = function(alreadySeen) {
+  return Question.findAll(
+    getOptionsForFindAllIdsNotSeen(alreadySeen)
+  ).then(function(questions) {
+    if(questions.length == 0) {
+      return null;
+    }
+    var index = randomInt(0, questions.length);
+    return questions[0];
+  });
+};
+
+var getOptionsForFindAllIdsNotSeen = function(alreadySeen) {
+  var findAllOptions = {};
+  if(alreadySeen.length > 0) {
+    findAllOptions.where = {
+      id: { $notIn: alreadySeen }
+    }
+  }
+  return findAllOptions;
+};
+
+var randomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
 
 module.exports = QuestionController;
--- spec/controllers/question_spec.js
+++ spec/controllers/question_spec.js
@@ -1,17 +1,77 @@
 'use strict'
 
 var QuestionController = require('../../controllers/question.js');
+var db = require('../../models/db.js');
+var Question = db.question;
 
 describe('QuestionController', function() {
-  it('shows a question', function() {
-    expect(false).toBe(true);
-  });
+  describe('#show', function() {
+    beforeEach(function(done) {
+      var self = this;
 
-  it('renders a message when no questions are left', function() {
-    expect(false).toBe(true);
-  });
+      Question.destroy({ where: {} }).then(function() {
+        return self.createDefaultQuestion();
+      }).then(done);
+    });
 
-  it('doesn\'t show the same question twice', function() {
-    expect(false).toBe(true);
+    it('shows a question', function(done) {
+      QuestionController.show(
+        createMockRequest(),
+        createMockReponse('question/show', true, done)
+      );
+    });
+
+    it('doesn\'t show the same question twice', function(done) {
+      // If there is only one question, and we mark it as already seen,
+      // there shouldn't be a question in the response.
+      Question.findOne().then(function(q) {
+        var mockRequest = createMockRequest();
+        mockRequest.cookies.alreadySeen = [ q.id ];
+
+        QuestionController.show(
+          mockRequest,
+          createMockReponse('question/show', false, done)
+        );
+      });
+    });
+  })
+
+  describe('#answer', function() {
+    it('responds with a 404 when the questionId isn\t found', function() {
+      expect(true).toBe(false);
+    });
+
+    it('responds with a 404 when the answerId doesn\'t belong to the question', function() {
+      expect(true).toBe(false);
+    });
+
+    it('returns a new question on success', function() {
+      expect(true).toBe(false);
+    });
+
+    it('increments stats for timesAnswered and timesAsked', function() {
+      expect(true).toBe(false);
+    });
   });
 });
+
+var createMockRequest = function() {
+  return {
+    cookies: {}
+  }
+};
+
+var createMockReponse = function(view, hasQuestion, done) {
+  return {
+    render: function(view, data, callback) {
+      expect(view).toEqual('question/show');
+      if(hasQuestion) {
+        expect(data.question).not.toBeUndefined();
+        expect(data.question.Model.name).toEqual('question');
+      } else {
+        expect(data.question).toBeNull();
+      }
+      done();
+    }
+  };
+};
