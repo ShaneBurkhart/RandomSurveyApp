fe83ceafb25a7db378add6b11dd69bce8a992d68
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-16 04:44:22 -0600

    Added gulp task to compile js.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,13 +2,18 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 var bower = require('gulp-bower');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 
 var config = {
     imgFiles: "./assets/img/*",
     imgDest: "./public/img",
     sassFiles: "./assets/sass/*.scss",
     cssDest: "./public/css",
+    scriptFiles: "./assets/js/*.js",
+    scriptDest: "./public/js",
     bowerDir: './bower_components' 
 }
 
@@ -29,4 +34,18 @@ gulp.task('css', function() {
 
 gulp.task('css:watch', ['css'], function() {
   gulp.watch(config.sassFiles, ['css']);
+});
+
+gulp.task('js', function() {
+  gulp.src([
+      config.bowerDir  + '/jquery/dist/jquery.js',
+      config.scriptFiles
+    ])
+    .pipe(concat('main.min.js'))
+    //.pipe(uglify())
+    .pipe(gulp.dest(config.scriptDest));
+});
+
+gulp.task('js:watch', ['js'], function() {
+  gulp.watch(config.scriptFiles, ['js']);
 });
