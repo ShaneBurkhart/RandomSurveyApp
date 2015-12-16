062baa7e151828990f20fd94cc29c226bf93c56b
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-16 03:23:22 -0600

    Added gulp task to move images into public dir.

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,7 +5,8 @@ var sass = require('gulp-sass');
 var bower = require('gulp-bower');
 
 var config = {
-    sassPath: "./assets/sass",
+    imgFiles: "./assets/img/*",
+    imgDest: "./public/img",
     sassFiles: "./assets/sass/*.scss",
     cssDest: "./public/css",
     bowerDir: './bower_components' 
@@ -15,12 +16,17 @@ gulp.task('bower', function() {
   return bower();
 });
 
+gulp.task('img', function() {
+  gulp.src(config.imgFiles)
+    .pipe(gulp.dest(config.imgDest));
+});
+
 gulp.task('css', function() {
   gulp.src(config.sassFiles)
     .pipe(sass().on('error', console.error))
     .pipe(gulp.dest(config.cssDest));
 });
 
-gulp.task('css:watch', function() {
+gulp.task('css:watch', ['css'], function() {
   gulp.watch(config.sassFiles, ['css']);
 });
