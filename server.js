3843b054afa330ffc57e91701ebfa318a9fa123b
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-16 02:20:21 -0600

    Added basedir to app so we can have absolute paths in jade templates.

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 
 var express = require('express');
 var http = require('http');
+var path = require('path');
 var QuestionController = require('./controllers/question.js');
 
 var app = express();
+var templateDir = path.join(__dirname, 'templates');
 
 app.set('view engine', 'jade');
-app.set('views', __dirname + '/templates');
+app.set('views', templateDir);
+app.locals.basedir = templateDir;
 
 app.get('/', QuestionController.show);
 
