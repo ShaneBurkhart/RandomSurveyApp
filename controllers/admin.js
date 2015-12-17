030906deea0fee029150b2fffdf37ac416e03e95
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-17 02:41:32 -0600

    Finished admin#login and admin#showLogin with tests.

---
+++ controllers/admin.js
@@ -0,0 +1,56 @@
+'use strict'
+
+var db = require('../models/db.js');
+var Question = db.question;
+var Answer = db.answer;
+var Admin = db.admin;
+
+var AdminController = {
+  index: function(req, res) {
+    // Show all questions and have a link to a page that can create pages.
+    Question.findAll().then(function(questions) {
+      res.render('admin/index', {
+        questions: questions
+      });
+    });
+  },
+
+  new: function(req, res) {
+    // Show form to create questions with answers.
+  },
+
+  create: function(req, res) {
+    // POST for new.  Creates both questions and answers.
+  },
+
+  edit: function(req, res) {
+    // Show edit of question.  On that page, there will be answer editing.
+  },
+
+  update: function(req, res) {
+    // POST update for edit.  Updates both questions and answers.
+  },
+
+  delete: function(req, res) {
+    // Delete question
+  },
+
+  showLogin: function(req, res) {
+    res.render('admin/login');
+  },
+
+  login: function(req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    Admin.findOne({ where: { email: email } }).then(function(a) {
+      if(a && a.comparePassword(password)) {
+        res.redirect('/admin/questions');
+      } else {
+        res.render('admin/login', { admin: a });
+      }
+    });
+  }
+};
+
+module.exports = AdminController;
--- spec/controllers/admin_spec.js
+++ spec/controllers/admin_spec.js
@@ -1,1 +1,121 @@
 'use strict'
+
+var AdminController = require('../../controllers/admin.js');
+var db = require('../../models/db.js');
+var Sequelize = db.Sequelize;
+var Promise = Sequelize.Promise;
+var Question = db.question;
+var Answer = db.answer;
+var Admin = db.admin;
+
+describe('AdminController', function() {
+  beforeEach(function(done) {
+    var self = this;
+
+    Question.destroy({ where: {} }).then(function() {
+      return Promise.settle([
+        self.createDefaultQuestionWithAnswers(),
+        self.createDefaultQuestionWithAnswers()
+      ]);
+    }).then(done).catch(this.simpleCatch);
+  });
+
+  describe('#index', function() {
+    it('shows a list of questions', function(done) {
+      var mockRequest = createMockRequest();
+      var mockResponse = createMockResponse('admin/index', 200, null, function(data) {
+        expect(data.questions.length).toBe(2);
+      }, done);
+
+      AdminController.index(mockRequest, mockResponse);
+    });
+  });
+
+  //describe('#new', function() {
+
+  //});
+  //describe('#create');
+  //describe('#edit');
+  //describe('#update');
+  //describe('#delete');
+
+  describe('#showLogin', function() {
+    it('renders the login form', function(done) {
+      var mockRequest = createMockRequest();
+      var mockResponse = createMockResponse('admin/login', 200, null, null, done);
+
+      AdminController.showLogin(mockRequest, mockResponse);
+    });
+  });
+
+  describe('#login', function() {
+    beforeEach(function(done) {
+      this.createDefaultAdmin().then(done);
+    });
+
+    it('logs in and redirects with correct credentials', function(done) {
+      var mockRequest = createMockRequest();
+      var mockResponse = createMockResponse(null, 200, '/admin/questions', null, done);
+
+      mockRequest.body.email = 'user@example.com'
+      mockRequest.body.password = 'password'
+
+      AdminController.login(mockRequest, mockResponse);
+    });
+
+    it('renders the login form with invalid email', function(done) {
+      var mockRequest = createMockRequest();
+      var mockResponse = createMockResponse('admin/login', 200, null, function(data) {
+        expect(data.admin).not.toBeUndefined();
+      }, done);
+
+      mockRequest.body.email = 'wrongemail@example.com'
+      mockRequest.body.password = 'password'
+
+      AdminController.login(mockRequest, mockResponse);
+    });
+
+    it('renders the login form with wrong password', function(done) {
+      var mockRequest = createMockRequest();
+      var mockResponse = createMockResponse('admin/login', 200, null, function(data) {
+        expect(data.admin).not.toBeUndefined();
+      }, done);
+
+      mockRequest.body.email = 'user@example.com'
+      mockRequest.body.password = 'wrong password'
+
+      AdminController.login(mockRequest, mockResponse);
+    });
+  });
+});
+
+var createMockRequest = function() {
+  return {
+    body: {},
+    session: {}
+  };
+}
+
+var createMockResponse = function(expectedView, expectedStatus, redirectUrl, checkData, done) {
+  return {
+    statusCode: 200,
+
+    status: function(code) {
+      this.statusCode = code;
+    },
+
+    render: function(view, data) {
+      expect(view).toEqual(expectedView);
+      expect(this.statusCode).toEqual(expectedStatus);
+      if(checkData) {
+        checkData(data);
+      }
+      done();
+    },
+
+    redirect: function(path) {
+      expect(path).toEqual(redirectUrl);
+      done();
+    }
+  };
+}
