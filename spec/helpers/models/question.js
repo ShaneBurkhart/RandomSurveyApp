bb62ba9ecafb991dd8dcd989cec7d2a7a295fcd0
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-17 21:39:29 -0600

    Changed createDefaultQuestionWithAnswers to return a question with answers included.

--- spec/helpers/models/question.js
+++ spec/helpers/models/question.js
@@ -3,6 +3,7 @@
 var db = require('../../../models/db.js');
 var Promise = db.Sequelize.Promise;
 var Question = db.question;
+var Answer = db.answer;
 
 beforeEach(function() {
   this.checkQuestionIsNotValid = function(q, done) {
@@ -16,12 +17,19 @@ beforeEach(function() {
 
   this.createDefaultQuestionWithAnswers = function() {
     var self = this;
+    var questionId = null;
 
     return this.createDefaultQuestion().then(function(q) {
+      questionId = q.id;
       return Promise.settle([
-        self.createAnswer('Yes', q.id),
-        self.createAnswer('No', q.id)
+        self.createAnswer('Yes', questionId),
+        self.createAnswer('No', questionId)
       ]);
+    }).then(function(results) {
+      // We are requerying for simplicity.  There is probably a way to get
+      // the question with answers, but I don't know enough about Sequlize to
+      // do it.  Performance doesn't matter since this is a test.
+      return Question.findById(questionId, { include: [ Answer ] });
     });
   };
 
