a29896049e52e3f328a31e4daf66513d2a800707
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-18 05:50:13 -0600

    Added request test for admin paths.  They are to test that the admin auth middleware is working properly.

--- spec/integration/admin_spec.js
+++ spec/integration/admin_spec.js
@@ -6,20 +6,35 @@ var session = require('supertest-session');
 var testSession = null;
 var question = null;
 
+var createQuestionData = { question: {
+  question: 'Do you like pizza?',
+  answers: [
+    { answer: 'Yes' },
+    { answer: 'No' }
+  ]
+} };
+
+var updateQuestionData = { question: {
+  question: 'Do you like pizza?',
+  answers: [
+    { id: 1, answer: 'Yes' },
+    { id: 2, answer: 'No' }
+  ]
+} };
+
 describe('Admin routes', function() {
   beforeAll(function() {
     testSession = session(app);
   });
 
-  describe('when Admin not logged in', function() {
-    beforeEach(function(done) {
-      this.createDefaultQuestion().then(function(q) {
-        question = q;
-        done();
-      });
+  beforeEach(function(done) {
+    this.createDefaultQuestionWithAnswers().then(function(q) {
+      question = q;
+      done();
     });
+  });
 
-    // This shouldn't be passing...
+  describe('when Admin not logged in', function() {
     describe('GET /admin', function() {
       it('returns 200', function(done) {
         testGET(testSession, '/admin', 200, done);
@@ -27,8 +42,8 @@ describe('Admin routes', function() {
     });
 
     describe('POST /admin/login', function() {
-      it('returns 404', function(done) {
-        testPOST(testSession, '/admin/login', 200, done);
+      it('returns 200', function(done) {
+        testPOST(testSession, '/admin/login', 200, null, done);
       });
     });
 
@@ -46,7 +61,7 @@ describe('Admin routes', function() {
 
     describe('POST /admin/questions', function() {
       it('returns 404', function(done) {
-        testPOST(testSession, '/admin/questions', 404, done);
+        testPOST(testSession, '/admin/questions', 404, createQuestionData, done);
       });
     });
 
@@ -60,14 +75,14 @@ describe('Admin routes', function() {
     describe('POST /admin/questions/:id/update', function() {
       it('returns 404', function(done) {
         // Id is arbitrary here since we get redirected in middleware.
-        testPOST(testSession, '/admin/questions/1/update', 404, done);
+        testPOST(testSession, '/admin/questions/1/update', 404, updateQuestionData, done);
       });
     });
 
     describe('POST /admin/questions/:id/delete', function() {
       it('returns 404', function(done) {
         // Id is arbitrary here since we get redirected in middleware.
-        testPOST(testSession, '/admin/questions/1/delete', 404, done);
+        testPOST(testSession, '/admin/questions/1/delete', 404, null, done);
       });
     });
   });
@@ -77,6 +92,56 @@ describe('Admin routes', function() {
       login(testSession, done);
     });
 
+    describe('GET /admin', function() {
+      it('returns 200', function(done) {
+        testGET(testSession, '/admin', 302, done);
+      });
+    });
+
+    describe('POST /admin/login', function() {
+      it('returns 302', function(done) {
+        testPOST(testSession, '/admin/login', 302, null, done);
+      });
+    });
+
+    describe('GET /admin/questions', function() {
+      it('returns 200', function(done) {
+        testGET(testSession, '/admin/questions', 200, done);
+      });
+    });
+
+    describe('GET /admin/questions/new', function() {
+      it('returns 200', function(done) {
+        testGET(testSession, '/admin/questions/new', 200, done);
+      });
+    });
+
+    describe('POST /admin/questions', function() {
+      it('returns 302', function(done) {
+        testPOST(testSession, '/admin/questions', 302, createQuestionData, done);
+      });
+    });
+
+    describe('GET /admin/questions/:id/edit', function() {
+      it('returns 200', function(done) {
+        // Id is arbitrary here since we get redirected in middleware.
+        testGET(testSession, '/admin/questions/' + question.id + '/edit', 200, done);
+      });
+    });
+
+    describe('POST /admin/questions/:id/update', function() {
+      it('returns 302', function(done) {
+        // Id is arbitrary here since we get redirected in middleware.
+        testPOST(testSession, '/admin/questions/' + question.id + '/update', 302, updateQuestionData, done);
+      });
+    });
+
+    describe('POST /admin/questions/:id/delete', function() {
+      it('returns 302', function(done) {
+        // Id is arbitrary here since we get redirected in middleware.
+        testPOST(testSession, '/admin/questions/' + question.id + '/delete', 302, null, done);
+      });
+    });
   });
 });
 
@@ -86,22 +151,24 @@ var testGET = function(sess, path, statusCode, done) {
       .end(createErrorCheck(done));
 };
 
-var testPOST = function(sess, path, statusCode, done) {
+var testPOST = function(sess, path, statusCode, data, done) {
     sess.post(path)
+      .send(data)
       .expect(statusCode)
       .end(createErrorCheck(done));
 };
 
 var login = function(sess, done) {
-  sess.post('/admin/login')
-    .send({ email: "shaneburkhart@gmail.com", password: "password" })
-    .expect(200)
-    .end(createErrorCheck(done));
+  //createDefaultAdmin().then(function() {
+    sess.post('/admin/login')
+      .send({ email: "user@example.com", password: "password" })
+      .expect(302)
+      .end(createErrorCheck(done));
+  //});
 }
 
 var createErrorCheck = function(done) {
   return function(err, res) {
-    console.log(res.statusCode);
     if(err) {
       expect(err).toBeUndefined();
     }
