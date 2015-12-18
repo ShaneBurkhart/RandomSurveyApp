1a556473d6d7b89e9cb6f8eb4971e619dae0e2d1
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-17 21:54:37 -0600

    Refactored answers model spec.

--- spec/helpers/models/answer.js
+++ spec/helpers/models/answer.js
@@ -4,6 +4,17 @@ var db = require('../../../models/db.js');
 var Answer = db.answer;
 
 beforeEach(function() {
+  this.checkAnswerIsNotValid = function(answer, qId, done) {
+    this.createAnswer(answer, qId).then(function(a) {
+      expect(a).toBeUndefined();
+      done();
+    }).catch(this.createCheckHasErrorsCallback(done));
+  };
+
+  this.createDefaultAnswer = function(qId) {
+    return this.createAnswer('Yes', qId);
+  };
+
   this.createAnswer = function(a, qId) {
     return Answer.create({
       answer: a,
--- spec/models/answer_spec.js
+++ spec/models/answer_spec.js
@@ -6,101 +6,35 @@ var Question = db.question;
 
 describe('Answer', function() {
   it('saves a record correctly', function(done) {
-    var answer = Answer.build({
-      answer: 'Some answer',
-      questionId: 1
-    });
-
-    answer.save().then(function(q) {
-      expect(q.isNewRecord).toBe(false);
+    this.createDefaultAnswer(1).then(function(a) {
+      expect(a.isNewRecord).toBe(false);
       done();
-    }).catch(function(err) {
-      expect(err).toBeUndefined();
-      done();
-    });
+    }).catch(this.createCheckHasNoErrorsCallback(done));
   })
 
   it('belongs to a question', function(done) {
-    var question = Question.build({
-      question: "How are you?"
-    });
 
-    question.save().then(function(q) {
-      return Answer.build({
-        answer: 'Some answer',
-        questionId: q.id
-      }).save();
-    }).then(function(a) {
-      expect(a.isNewRecord).toBe(false);
-      expect(a.getQuestion()).not.toBeUndefined();
+    this.createDefaultQuestionWithAnswers().then(function(q) {
+      var answer = q.answers[0];
+      expect(answer.isNewRecord).toBe(false);
+      expect(answer.getQuestion()).not.toBeUndefined();
       done();
-    }).catch(function(err) {
-      expect(err).toBeUndefined();
-      done();
-    });
+    }).catch(this.createCheckHasNoErrorsCallback(done));
   });
 
   it('doesn\'t allow null answer', function(done) {
-    var answer = Answer.build({
-      answer: null,
-      questionId: 1
-    });
-
-    // We have to actually save the record to check the allowNull validation.  It isn't
-    // an instance validator and instead a Schema validator, so it doesn't fail until we
-    // try to save to the db.
-    answer.save().then(function(q) {
-      // This is just to get a more clear error message. If we get here, the
-      // test has failed.
-      expect(q).not.toBeUndefined();
-      done();
-    }).catch(function(err) {
-      expect(err).not.toBeUndefined();
-      done();
-    });
+    this.checkAnswerIsNotValid(null, 1, done);
   });
 
   it('doesn\'t allow empty answer', function(done) {
-    var answer = Answer.build({
-      answer: '',
-      questionId: 1
-    });
-
-    answer.validate().then(function(errors) {
-      expect(errors).not.toBeUndefined();
-      done();
-    });
+    this.checkAnswerIsNotValid('', 1, done);
   });
 
   it('doesn\'t allow null questionId', function(done) {
-    var answer = Answer.build({
-      answer: 'Some answer',
-      questionId: null
-    });
-
-    // We have to actually save the record to check the allowNull validation.  It isn't
-    // an instance validator and instead a Schema validator, so it doesn't fail until we
-    // try to save to the db.
-    answer.save().then(function(q) {
-      // This is just to get a more clear error message. If we get here, the
-      // test has failed.
-      expect(q).not.toBeUndefined();
-      done();
-    }).catch(function(err) {
-      expect(err).not.toBeUndefined();
-      done();
-    });
+    this.checkAnswerIsNotValid('Yes', null, done);
   });
 
   it('doesn\'t allow questionId less than 1', function(done) {
-    var answer = Answer.build({
-      answer: 'Some answer',
-      questionId: 0
-    });
-
-    answer.validate().then(function(errors) {
-      expect(errors).not.toBeUndefined();
-      done();
-    });
+    this.checkAnswerIsNotValid('Yes', 0, done);
   });
 });
