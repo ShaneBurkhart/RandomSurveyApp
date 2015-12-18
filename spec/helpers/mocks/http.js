57babd07955dcfa21ae64419fc24af47a0ed5532
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-17 21:38:16 -0600

    Moved mock response and request to http.js

--- spec/helpers/mocks/http.js
+++ spec/helpers/mocks/http.js
@@ -1,25 +1,52 @@
 'use strict'
 
 beforeEach(function() {
-  this.simpleCatch = function(err) {
-    console.log(err);
-  };
-
-  this.createMockResponseRenderWithAssertions = function(done) {
+  this.createMockRequest = function() {
     return {
-      render: function(view, data, callback) {
-        expect(mockResponse.renderView).toEqual('question/show');
-        expect(mockResponse.renderData).toEqual(jasmine.any(Question));
+      cookies: {},
+      params: {},
+      body: {},
+      session: {}
+    };
+  }
 
-        if(this.renderArgumentsNum == 2) {
-          // The data attr is optional
-          this.renderCallback = arguments[1];
-        } else {
-          this.renderData = arguments[1];
-          this.renderCallback = arguments[2];
+  this.createMockResponse = function(expectedView, expectedStatus, redirectUrl, checkData, done) {
+    return {
+      statusCode: 200,
+      cookies: {},
+
+      cookie: function(name, value, options) {
+        this.cookies[name] = value;
+      },
+
+      status: function(code) {
+        this.statusCode = code;
+        return this;
+      },
+
+      redirect: function(path) {
+        this.status(302);
+        expect(this.statusCode).toEqual(expectedStatus);
+        expect(path).toEqual(redirectUrl);
+        done();
+      },
+
+      render: function(view, data) {
+        expect(view).toEqual(expectedView);
+        expect(this.statusCode).toEqual(expectedStatus);
+        if(checkData) {
+          checkData(data);
+        }
+        done();
+      },
+
+      json: function(data) {
+        expect(this.statusCode).toEqual(expectedStatus);
+        if(checkData) {
+          checkData(data);
         }
         done();
       }
     };
-  }
+  };
 });
