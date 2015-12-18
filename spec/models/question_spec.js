3190b8d4577480d2d8aa1c19c01018a36e7c583e
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-17 21:41:56 -0600

    Refactored question model spec.

--- spec/models/question_spec.js
+++ spec/models/question_spec.js
@@ -5,26 +5,17 @@ var db = require('../../models/db.js');
 var Answer = db.answer;
 describe('Question', function() {
   it('saves a record correctly', function(done) {
-    this.createDefaultQuestion('My name is Ron Burgundy?').then(function(q) {
+    this.createDefaultQuestion().then(function(q) {
       expect(q.isNewRecord).toBe(false);
       done();
     }).catch(this.createCheckHasNoErrorsCallback(done));
   });
 
   it('has many answers', function(done) {
-    var question;
     var self = this;
-    this.createDefaultQuestion().then(function(q) {
-      // There is probably a better way to save the question for later promises,
-      // but this will do for now.
-      question = q;
-      return self.createAnswer('Some answer', question.id);
-    }).then(function(a1) {
-      return self.createAnswer('Some other answer', question.id);
-    }).then(function(a2) {
-      return question.getAnswers();
-    }).then(function(answers) {
-      expect(answers.length).toBe(2);
+
+    this.createDefaultQuestionWithAnswers().then(function(q) {
+      expect(q.answers.length).toBe(2);
       done();
     }).catch(this.createCheckHasNoErrorsCallback(done));
   });
