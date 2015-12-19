355b0ec1427d10948bf99fb02105ae55b7832e46
Shane Burkhart <shaneburkhart@gmail.com>
2015-12-18 18:51:23 -0600

    Added cascade for delete on question model so answers to the question will get deleted too.

--- models/question.js
+++ models/question.js
@@ -14,7 +14,9 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        Question.hasMany(models.answer);
+        Question.hasMany(models.answer, {
+          onDelete: "CASCADE",
+        });
       }
     }
   });
